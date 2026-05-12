350bb35e2ce97761852edeb616ec007259dd7739
Goh Ching Yee - 2025 <25006756@siswa.um.edu.my>
2026-05-12 17:47:55 +0800

    Skeleton

---
+++ src/main/java/wia1002_group_assignment/emergency_medical_dispatch/EmergencyMedicalDispatchApplication.java
@@ -0,0 +1,13 @@
+package wia1002_group_assignment.emergency_medical_dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyMedicalDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyMedicalDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wia1002_group_assignment/emergency_medical_dispatch/EmergencyMedicalDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package wia1002_group_assignment.emergency_medical_dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyMedicalDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
